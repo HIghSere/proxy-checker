b25bb48a945494a147cafd535551a2ea2a6b03ca
melatnix <144571368+HIghSere@users.noreply.github.com>
2024-07-29 23:59:22 +0900

    Delete dist/util/ua-gen.ts

--- dist/util/ua-gen.ts
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-
-export function UAGen() {
-    const userAgents = fs.readFileSync("data/userAgents.txt", "utf-8");
-    return userAgents[Math.floor(Math.random() * userAgents.length)];
-}
