ef72b66725f0a3df43d4e411b48a69a25266a2a7
melatnix <144571368+HIghSere@users.noreply.github.com>
2024-07-30 09:27:09 +0900

    Create ua-gen.js

---
+++ dist/util/ua-gen.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UAGen = UAGen;
+const fs_1 = __importDefault(require("fs"));
+function UAGen() {
+    const userAgents = fs_1.default.readFileSync("data/user-agents.txt", "utf-8").split("\n");
+    return userAgents[Math.floor(Math.random() * userAgents.length)];
+}
